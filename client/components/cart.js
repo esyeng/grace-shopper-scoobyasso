9465be8bb36c052bd2218f911796afbf1d6e7a90
Robert Harkov <robertharkov@gmail.com>
2020-08-06 15:07:41 -0400

    trying to display cart

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {fetchCart} from '../store/cart'
+import {connect} from 'react-redux'
 
-const Cart = () => {
-  return <div />
+export class Cart extends Component {
+  componentDidMount() {
+    this.props.getCart(1)
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.props.cart.userId}</h1>
+        <h1>HELLO FROM CART</h1>
+      </div>
+    )
+  }
 }
 
-module.exports = Cart
+const mapStateToProps = state => {
+  return {
+    cart: state.cart
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getCart: id => dispatch(fetchCart(id))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
