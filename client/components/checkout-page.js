8dabea91d96b16c9bc669dc8bdd6da9a90ba77eb
Anderson Yoon <snowglow22y@gmail.com>
2020-08-10 21:45:01 -0400

    feat(checkout)updated data being sent to place order thunk

--- client/components/checkout-page.js
+++ client/components/checkout-page.js
@@ -1,52 +1,100 @@
 import React, {Component} from 'react'
 // import {fetchCart, modifyCart} from '../store/cart'
 import {connect} from 'react-redux'
-import Address from './address-form'
+import {Address} from './address-form'
+import {Payment} from './payment-form'
 
 class Checkout extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      isChecked: false
+      // isChecked: false
+      street: '',
+      city: '',
+      zip: '',
+      state: '',
+      country: '',
+      ccNum: '',
+      cvc: '',
+      expDate: '',
+      ccZip: ''
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleChange() {
-    this.setState({
-      isChecked: !this.state.isChecked
+  async handleChange(e) {
+    await this.setState({
+      [e.target.name]: e.target.value
     })
+  }
+
+  handleSubmit() {
+    const {placeOrder, user} = this.props
+    const city = this.state.city
+    const zip = this.state.zip
+    const state = this.state.state
+    const country = this.state.country
+    const street = this.state.street
+    const checkoutObj = {
+      address: {
+        city,
+        zip,
+        state,
+        street,
+        country
+      },
+      user: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName
+      },
+      payment: {
+        ccNum: this.state.ccNum,
+        cvc: this.state.cvc,
+        expDate: this.state.expDate,
+        ccZip: this.state.ccZip
+      }
+    }
+    placeOrder(checkoutObj)
   }
 
   render() {
     return (
       <div>
         <div name="checkbox">
-          <label className="isSameAsShip">
+          {/* <label className="isSameAsShip">
             Billing address same as shipping address
             <input
               type="checkbox"
               defaultChecked={this.state.isChecked}
               onChange={this.handleChange}
             />
-          </label>
+          </label> */}
         </div>
-        <Address method="Shipping" />
-        {this.state.isChecked ? '' : <Address method="Billing" />}
+        <Address
+          method="Shipping"
+          submit={this.handleSubmit}
+          change={this.handleChange}
+        />
+        <Payment change={this.handleChange} />
+        <button onClick={this.handleSubmit}>Place Order</button>
+        {/* {this.state.isChecked ? '' : <Address method="Billing" />} */}
       </div>
     )
   }
 }
 
-// const mapState = state => {
-//   return {
-//     method: state.method
-//   }
-// }
-const mapDispatch = dispatch => {
+const mapState = state => {
   return {
-    //
+    user: state.user
   }
 }
 
-export default connect(null, mapDispatch)(Checkout)
+const mapDispatch = dispatch => {
+  return {
+    placeOrder: object => dispatch(placeOrder(object))
+  }
+}
+
+export default connect(mapState, mapDispatch)(Checkout)
